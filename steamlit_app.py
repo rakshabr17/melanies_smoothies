8050f141c3320eb0505cc9c5eb0363044c33304b
rakshabr17 <rakshabr17@gmail.com>
2025-08-20 15:13:56 +0530

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,6 +1,8 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+cnx=st.connection("Snowflake")
+session = cnx.session()
 
 
 # Write directly to the app
@@ -9,8 +11,7 @@ st.write(
   """Choose the fruits you want in your custom Smoothie!"""
 )
 
-cnx=st.connection("Snowflake")
-session = cnx.session()
+
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_Name'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
